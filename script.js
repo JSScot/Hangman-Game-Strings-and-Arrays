dd87da2494e31270f59747f105ce680970cb8df0
JSScot <153024062+JSScot@users.noreply.github.com>
2024-01-30 19:29:03 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,103 @@
+// words 
+
+const introImage = document.getElementById("intro");
+
+const words = ["javascript", "html", "css", "python", "java", "ruby", "coding", "swift", "kotlin" , "strawberry", "mango" , "sleepy"];
+
+// random word from the array
+const word = words[Math.floor(Math.random() * words.length)];
+
+//Break up word into letters
+let letters = word.split("");
+
+// initialize the userput variable
+let userinput = null;
+
+
+// specify the letters in the user's alphabet
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+// saving the letters 
+let word2_arr = [], word2 = "";
+// track number of tries
+let tryCount = 0;
+
+for (let index = 0; index < letters.length; index++) {
+
+  word2_arr[index] = "_";
+
+}
+
+word2 = word2_arr.join("");
+
+
+  while (word2 != word)
+    {
+      userinput = prompt("Guess a letter (in lowercase) for this word or click Cancel to end the game:\n" + word2_arr);
+      if (userinput)
+        if (userinput.length == 1) {
+          tryCount++;
+          console.log(userinput);
+          userinput = userinput.toLowerCase();
+
+            if (letters.indexOf(userinput) >= 0) {
+
+              for (let index = 0; index < letters.length; index++) {
+
+                if (letters[index] == userinput) {
+
+                  word2_arr[index] = userinput;
+                  word2 = word2_arr.join("");
+                  console.log(userinput);
+
+                }
+
+              }
+
+          }
+        }
+
+
+
+
+
+
+    }
+
+    if(tryCount <= word.length+2)
+    {
+    alert("Whoa that was great! Did you cheat?\nThe word was: " + word + " \nThat took you " + tryCount + " tries" );
+    }
+else
+    {
+      alert("You guessed it! \nThe word was: " + word + " \nThat took you " + tryCount + " tries" );
+    }
+
+/*switch (words.indexOf(chosenword)){
+
+  case 0:
+
+   introImage.remove();
+ document.getElementById("prize").src="https://i.pinimg.com/564x/77/ce/76/77ce769c0ce57d1e8e86c047c529aa08.jpg";
+
+    
+  break;
+  case 1:
+
+    document.getElementById("prize").src="https://i.pinimg.com/564x/77/ce/76/77ce769c0ce57d1e8e86c047c529aa08.jpg";
+
+
+    break;
+    default:
+    alert(words.indezOf(chosenword)
+
+      break:
+    )
+    
+
+
+    }
+    */
+    
+
+
